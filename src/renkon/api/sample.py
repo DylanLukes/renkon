c311e21906cb8a80487e4ccaa1522231ab6946cd
Dylan Lukes <lukes.dylan@gmail.com>
2023-08-09 20:02:40 -0700

    sampler library

--- src/renkon/api/sample.py
+++ src/renkon/api/sample.py
@@ -1,3 +1,3 @@
-from renkon.infer.sampling import const, frac, full, null
+from renkon.infer.sampling import const, frac, full, null, slice_
 
-__all__ = ["const", "frac", "full", "null"]
+__all__ = ["const", "frac", "slice_", "full", "null"]
--- src/renkon/infer/sampling.py
+++ src/renkon/infer/sampling.py
@@ -6,7 +6,6 @@ from typing import Protocol
 
 import numpy as np
 import polars as pl
-from loguru import logger
 
 _rng = np.random.default_rng()
 
@@ -19,27 +18,29 @@ class Sampler(Protocol):
     used with polars. Using expressions allows polars to optimize more.
     """
 
+    @property
     @abstractmethod
     def indices(self) -> pl.Expr:
         """
 
         Usage example::
 
-            sampler = rk.sample.const(k=10)
-            df.select(pl.col("...").take(sampler.indices()))
+            sample = rk.sample.const(k=10)
 
+            df.select(pl.col("...").take(sample.indices()))
         :return: an expression leading to a series of integers in the range [0, n).
         """
         ...
 
+    @property
     @abstractmethod
     def mask(self) -> pl.Expr:
         """
 
         Usage example::
 
-            sampler = rk.sample.const(k=10)
-            df.select(pl.col("...").filter(sampler.mask()))
+            sample = rk.sample.const(k=10)
+            df.select(pl.col("...").filter(sample.mask()))
 
         Or, shorthand for all columns::
 
@@ -50,13 +51,24 @@ class Sampler(Protocol):
         ...
 
 
+class _MaskFromIndicesMixin(Sampler, Protocol):
+    """Default implementation of mask() in terms of indices()."""
+
+    @property
+    def mask(self) -> pl.Expr:
+        row_nrs = pl.int_range(0, pl.count())
+        return pl.when(row_nrs.is_in(self.indices)).then(True).otherwise(False)
+
+
 @dataclass(kw_only=True)
 class FullSampler(Sampler):
     """Samples all data."""
 
+    @property
     def indices(self) -> pl.Expr:
         return pl.int_range(0, pl.count())
 
+    @property
     def mask(self) -> pl.Expr:
         return pl.lit(True)
 
@@ -65,56 +77,55 @@ class FullSampler(Sampler):
 class NullSampler(Sampler):
     """Samples no data."""
 
+    @property
     def indices(self) -> pl.Expr:
         return pl.lit([])
 
+    @property
     def mask(self) -> pl.Expr:
         return pl.lit(False)
 
 
 @dataclass(kw_only=True)
-class ConstSampler(Sampler):
+class ConstSampler(_MaskFromIndicesMixin, Sampler):
     """Samples a constant k-subset of the data."""
 
     k: int
 
+    @property
     def indices(self) -> pl.Expr:
-        def choose(n):
+        def choose(n: int) -> pl.Series:
             # _rng.choice is constant time in n.
             return pl.Series(_rng.choice(n, self.k, replace=False))
 
         return pl.count().apply(choose).explode().alias("")
 
-    def mask(self) -> pl.Expr:
-        row_nrs = pl.int_range(0, pl.count())
-        return pl.when(row_nrs.is_in(self.indices())).then(True).otherwise(False)
+
+@dataclass(kw_only=True)
+class SliceSampler(_MaskFromIndicesMixin, Sampler):
+    """Samples a specific slice of the data. Useful for testing!"""
+
+    offset: int
+    length: int
+
+    @property
+    def indices(self) -> pl.Expr:
+        return pl.int_range(self.offset, self.offset + self.length)
 
 
 @dataclass(kw_only=True)
-class FractionSampler(Sampler):
+class FractionSampler(_MaskFromIndicesMixin, Sampler):
     """Samples a fraction f of the data."""
 
     f: float
 
+    @property
     def indices(self) -> pl.Expr:
         return pl.int_range(0, pl.count()).sample(fraction=self.f)
 
-    def mask(self) -> pl.Expr:
-        row_nrs = pl.int_range(0, pl.count())
-        return pl.when(row_nrs.is_in(self.indices())).then(True).otherwise(False)
 
-
-def full() -> FullSampler:
-    return FullSampler()
-
-
-def null() -> NullSampler:
-    return NullSampler()
-
-
-def const(*, k: int) -> ConstSampler:
-    return ConstSampler(k=k)
-
-
-def frac(*, f: float) -> FractionSampler:
-    return FractionSampler(f=f)
+full = FullSampler
+null = NullSampler
+const = ConstSampler
+slice_ = SliceSampler
+frac = FractionSampler
