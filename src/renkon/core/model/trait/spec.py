4fcd9cd082d97bd58a20846efb43495059040d20
Dylan Lukes <lukes.dylan@gmail.com>
2024-08-20 18:08:12 -0700

    add TraitSpec.commutors

--- src/renkon/core/model/trait/spec.py
+++ src/renkon/core/model/trait/spec.py
@@ -4,9 +4,9 @@
 
 __TRAIT_INFO__ = "__trait_info__"
 
-from typing import Annotated
+from typing import Annotated, Self
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, model_validator
 
 from renkon.core.model.trait.kind import TraitKind
 from renkon.core.model.trait.pattern import TraitPattern
@@ -17,13 +17,21 @@ type TraitId = str
 
 class TraitSpec(BaseModel):
     """
-    Model representing the descriptive identity of a trait.
+    Model for the descriptive data for a trait.
 
     This is as opposed to the behavioral functionality (e.g. inference, scoring)
-    found in :class:`~renkon.core.trait.Trait`.
+    found in :class:`renkon.core.trait.Trait`.
+
+    :param id:        unique identifier of the trait (generally fully-qualified class name)
+    :param name:      human-readable name of the trait
+    :param kind:      sort of the trait, e.g. "algebraic", "model", etc.
+    :param pattern:   string pattern of the trait, metavars (for columns) in uppers, inferred params in lowers
+    :param commutors: list of sets of metavars that can commute (default: [])
+    :param typevars:  declaration of bound type variables for use in typings (default: {})
+    :param typings:   declarations of types, may be either Renkon types or typevars (default: {})
 
     >>> trait = TraitSpec.model_validate_json('''{
-    ...     "id": "renkon.core.trait.linear.Linear2",
+    ...     "id": "renkon.core.doctest.traits.Linear2",
     ...     "name": "Linear Regression (2D)",
     ...     "kind": "model",
     ...     "pattern": "{Y} = {a}*{X} + {b}",
@@ -35,16 +43,27 @@ class TraitSpec(BaseModel):
     ...     }
     ... }''')
 
-    :param id: the unique identifier of the trait.
-    :param name: the human-readable name of the trait.
-    :param kind: the sort of the trait, e.g. "algebraic", "model", etc.
-    :param pattern: the string pattern of the trait
+    >>> trait = TraitSpec.model_validate({
+    ...     "id": "renkon.core.doctest.traits.Equal",
+    ...     "name": "Equal",
+    ...     "kind": "logical",
+    ...     "pattern": "{A} = {B}",
+    ...     "commutors": [{"A", "B"}],
+    ...     "typevars": {
+    ...         "T": "equatable",
+    ...      },
+    ...     "typings": {
+    ...         "A": "T",
+    ...         "B": "T"
+    ...     }
+    ... })
     """
 
     id: TraitId
     name: str
     kind: TraitKind
     pattern: TraitPattern
+    commutors: list[set[str]] = []
     typevars: dict[str, Type] = {}
     typings: dict[str, Annotated[Type | str, Field(union_mode="left_to_right")]] = {}
 
@@ -56,21 +75,35 @@ class TraitSpec(BaseModel):
     def params(self) -> set[str]:
         return set(self.pattern.params)
 
-    # @model_validator(mode="after")
-    # def _check_typings_keys_in_pattern(self) -> Self:
-    #     for key, _ in self.typings.items():
-    #         if key not in self.metavars | self.params:
-    #             msg = f"Typing key '{key}' not found in pattern."
-    #             raise ValueError(msg)
-    #     return self
-    #
-    # @model_validator(mode="after")
-    # def _check_typings_vals_valid_types_or_typevars(self) -> Self:
-    #     for key, value in self.typings.items():
-    #         if isinstance(value, Type):
-    #             continue
-    #
-    #         if value not in self.typevars:
-    #             msg = f"Type variable typing '{value}' for '{key}' not found in typevars."
-    #             raise ValueError(msg)
-    #     return self
+    @model_validator(mode="after")
+    def _check_all_typed(self) -> Self:
+        for metavar in self.metavars:
+            if metavar not in self.typings:
+                msg = f"Metavariable '{metavar}' from '{self.pattern}' is not typed."
+                raise ValueError(msg)
+
+        for param in self.params:
+            if param not in self.typings:
+                msg = f"Parameter '{param}' from '{self.pattern}' is not typed."
+                raise ValueError(msg)
+
+        return self
+
+    @model_validator(mode="after")
+    def _check_typings_keys_in_pattern(self) -> Self:
+        for key, _ in self.typings.items():
+            if key not in self.metavars | self.params:
+                msg = f"Typing key '{key}' not found in pattern."
+                raise ValueError(msg)
+        return self
+
+    @model_validator(mode="after")
+    def _check_typings_vals_valid_types_or_typevars(self) -> Self:
+        for key, value in self.typings.items():
+            if isinstance(value, Type):
+                continue
+
+            if value not in self.typevars:
+                msg = f"Type variable typing '{value}' for '{key}' not found in typevars."
+                raise ValueError(msg)
+        return self
---
+++ tests/renkon/core/model/trait/test_pattern.py
@@ -0,0 +1,46 @@
+# SPDX-FileCopyrightText: 2024-present Dylan Lukes <lukes.dylan@gmail.com>
+#
+# SPDX-License-Identifier: BSD-3-Clause
+import pytest
+from pydantic import TypeAdapter
+
+from renkon.core.model import TraitPattern
+
+
+def test_trait_pattern_validation_errors():
+    ta = TypeAdapter(TraitPattern)
+    pattern = ta.validate_python("{Y} = {a}*{X} + {b}")
+    assert pattern.metavars == ["Y", "X"]
+    assert pattern.params == ["a", "b"]
+
+    with pytest.raises(ValueError, match="must be named"):
+        ta.validate_python("{} = {a}*{X} + {b}")
+
+    with pytest.raises(ValueError, match="must start with a letter"):
+        ta.validate_python("{123} = {a}*{X} + b")
+
+    with pytest.raises(ValueError, match="must be unique"):
+        ta.validate_python("{Y} = {a}*{Y} + b")
+
+
+def test_trait_pattern_format():
+    ta = TypeAdapter(TraitPattern)
+    pattern = ta.validate_python("{Y} = {a}*{X} + {b}")
+
+    # happy case
+    assert pattern.format(Y="money", X="time", a=3, b=4) == "money = 3*time + 4"
+
+    # positional arguments are forbidden
+    with pytest.raises(ValueError, match="does not accept positional arguments"):
+        pattern.format("money", "time", 3, 4)
+
+    # extra fields are forbidden when extra="forbid"
+    with pytest.raises(ValueError, match="extra fields are forbidden: {'c'}"):
+        pattern.format(Y="money", X="time", a=3, b=4, c=5)
+
+    # missing fields are forbidden when missing="forbid"
+    with pytest.raises(ValueError, match="missing fields are forbidden: {'b'}"):
+        pattern.format(Y="money", X="time", a=3)
+
+    # missing fields left as template fields when missing="partial"
+    assert pattern.format(Y="money", a=3, missing="partial") == "money = 3*{X} + {b}"
