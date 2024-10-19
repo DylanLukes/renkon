6c53602c80c207dcc1d709c7023348469aa923a8
Dylan Lukes <lukes.dylan@gmail.com>
2024-10-18 22:17:53 -0700

    maximize again

--- .github/scripts/setup_ubuntu.sh
+++ .github/scripts/setup_ubuntu.sh
@@ -5,10 +5,25 @@ sudo apt install -y -V ./apache-arrow-apt-source-latest-$(lsb_release --codename
 
 sudo apt update
 
+sudo apt install -y -V \
+  nvidia-cuda-toolkit
+
 # Based on: https://arrow.apache.org/install/
-sudo apt install -y -\
+sudo apt install -y -V \
   libre2-dev \
   libzstd-dev \
   liblz4-dev \
   libarrow-dev \
-  libparquet-dev
+  libarrow-glib-dev \
+  libarrow-acero-dev \
+  libarrow-dataset-dev \
+  libarrow-dataset-glib-dev \
+  libarrow-flight-dev \
+  libarrow-flight-glib-dev \
+  libarrow-flight-sql-dev \
+  libarrow-flight-sql-glib-dev \
+  libarrow-cuda-dev \
+  libgandiva-dev \
+  libgandiva-glib-dev \
+  libparquet-dev \
+  libparquet-glib-dev
