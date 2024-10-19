d2f79b37afda65c9b224a86eb2939eefb028878b
Dylan Lukes <lukes.dylan@gmail.com>
2024-10-18 22:04:18 -0700

    fix setup_ubuntu.sh

--- .github/scripts/setup_ubuntu.sh
+++ .github/scripts/setup_ubuntu.sh
@@ -11,13 +11,14 @@ sudo apt install -y -V libzstd-dev
 sudo apt install -y -V liblz4-dev
 sudo apt install -y -V libarrow-dev # For C++
 sudo apt install -y -V libarrow-glib-dev # For GLib (C)
+sudo apt install -y -V libarrow-acero-dev # For Apache Arrow Acero
 sudo apt install -y -V libarrow-dataset-dev # For Apache Arrow Dataset C++
 sudo apt install -y -V libarrow-dataset-glib-dev # For Apache Arrow Dataset GLib (C)
-sudo apt install -y -V libarrow-acero-dev # For Apache Arrow Acero
 sudo apt install -y -V libarrow-flight-dev # For Apache Arrow Flight C++
 sudo apt install -y -V libarrow-flight-glib-dev # For Apache Arrow Flight GLib (C)
 sudo apt install -y -V libarrow-flight-sql-dev # For Apache Arrow Flight SQL C++
 sudo apt install -y -V libarrow-flight-sql-glib-dev # For Apache Arrow Flight SQL GLib (C)
+sudo apt install -y -V libarrow-cuda-dev # For Apache Arrow CUDA
 sudo apt install -y -V libgandiva-dev # For Gandiva C++
 sudo apt install -y -V libgandiva-glib-dev # For Gandiva GLib (C)
 sudo apt install -y -V libparquet-dev # For Apache Parquet C++
